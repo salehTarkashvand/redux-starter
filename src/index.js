db74bf947b1ac2fdba0497e370bd646c917521a3
hamedtarkashvand <hamed.tarkashvand@gmail.com>
2024-02-11 20:10:41 +0330

    last sen 12 +1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log("Hello World!");
+import { compose , pipe } from 'loadsh/fp'
+import { Map } from 'immutable';
+const input = "   HAMED   ";
+
+const trim = str => str.trim()
+const wrapInDiv = str => `<div>${str}</div>`
+const wrapInSpan = (type) => (str) => `<${type}>${str}</${type}$>`;
+const toLowerCase = str => str.toLowerCase()
+const output =  wrapInDiv(toLowerCase(trim(input)))
+
+const pCompose = pipe(trim, toLowerCase, wrapInSpan('span'));
+
+
+console.log(pCompose(input));
+
+function sum(a, b) {
+  return a + b;
+}
+
+// refactor this method 
+
+function curryingSum(a) {
+    return function (b) {
+        return a + b
+    }
+}
+const argA = curryingSum(5);
+console.log(argA(5));
+
+// again refactors with arrow function
+const curryingSumV2 = a => b => a + b
+console.log(curryingSumV2(3)(3))
+
+console.log(sum(1 , 2));
