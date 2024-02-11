bfbee3c9194e6f49b5c1a914b94edbe3a2a97c3c
hamedtarkashvand <hamed.tarkashvand@gmail.com>
2024-02-11 20:22:42 +0330

    about package  immutable

--- src/index.js
+++ src/index.js
@@ -1,34 +1,13 @@
-import { compose , pipe } from 'loadsh/fp'
+
 import { Map } from 'immutable';
-const input = "   HAMED   ";
 
-const trim = str => str.trim()
-const wrapInDiv = str => `<div>${str}</div>`
-const wrapInSpan = (type) => (str) => `<${type}>${str}</${type}$>`;
-const toLowerCase = str => str.toLowerCase()
-const output =  wrapInDiv(toLowerCase(trim(input)))
+let person = Map({
+  name: 'hamed',
+});
 
-const pCompose = pipe(trim, toLowerCase, wrapInSpan('span'));
-
-
-console.log(pCompose(input));
-
-function sum(a, b) {
-  return a + b;
+const addPro = (object) => {
+  return object.set('age' , 30)
 }
 
-// refactor this method 
-
-function curryingSum(a) {
-    return function (b) {
-        return a + b
-    }
-}
-const argA = curryingSum(5);
-console.log(argA(5));
-
-// again refactors with arrow function
-const curryingSumV2 = a => b => a + b
-console.log(curryingSumV2(3)(3))
-
-console.log(sum(1 , 2));
+person = addPro(person)
+console.log(person.toJS());
